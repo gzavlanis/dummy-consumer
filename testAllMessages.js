e5a9240efe2f6147bd7763a7d861a7d763729c13
gzavlanis <gzavlanis@gmail.com>
2024-04-24 15:54:50 +0300

    test the odd values

---
+++ testAllMessages.js
@@ -0,0 +1,52 @@
+require("dotenv").config();
+const amqp = require("amqplib/callback_api");
+
+amqp.connect(process.env.RMQ_URI, (err, connection) => {
+  if (err) throw err;
+  console.log(`Connected to ${process.env.RMQ_URI}`);
+
+  connection.createChannel((err1, channel) => {
+    if (err1) throw err1;
+
+    let exchange = process.env.RMQ_EXCHANGE;
+    let key = process.env.RMQ_KEY;
+    let date = new Date().toLocaleString();
+
+    channel.assertExchange(exchange, "topic", { durable: true });
+
+    channel.assertQueue(
+      `zavlanis-consumer-queue ${date}`,
+      { exclusive: false, autoDelete: true },
+      (err2, q) => {
+        if (err2) throw err2;
+        console.log(`Waiting for messages. Press CTRL+C to exit.`);
+        channel.bindQueue(q.queue, exchange, key);
+        channel.consume(
+          q.queue,
+          (msg) => {
+            let response = [];
+            let message = JSON.parse(msg.content.toString());
+            if (message.body.fixture.eventId == 526877099) {
+              console.log(message.body.fixture.eventId);
+              let markets = message.body.markets;
+              if (markets && markets.length > 0) {
+                for (let i = 0; i < markets.length; i++) {
+                  response.push({ name: markets[i].name });
+                  for (let j = 0; j < markets[i].odds.length; j++) {
+                    response[i].odds = {
+                      name: markets[i].odds[j].name,
+                      value: markets[i].odds[j].value,
+                      odd: markets[i].odds[j].odd,
+                    };
+                  }
+                }
+                console.log(response);
+              }
+            }
+          },
+          { noAck: true }
+        );
+      }
+    );
+  });
+});
